8a72592d5e892a4a113b7000055e3d00839eb5f6
chusunhao <52926684+chusunhao@users.noreply.github.com>
2021-06-26 22:05:23 +0800

    Update train.py

--- pytorch_classification/Test5_resnet/train.py
+++ pytorch_classification/Test5_resnet/train.py
@@ -25,15 +25,14 @@ def main():
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
 
     data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
-    image_path = os.path.join(data_root, "data_set", "flower_data")  # flower data set path
+    image_path = os.path.join(data_root, "data_set", "dog_data")  # dog data set path
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
     train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
                                          transform=data_transform["train"])
     train_num = len(train_dataset)
 
-    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
-    flower_list = train_dataset.class_to_idx
-    cla_dict = dict((val, key) for key, val in flower_list.items())
+    dog_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in dog_list.items())
     # write dict into json file
     json_str = json.dumps(cla_dict, indent=4)
     with open('class_indices.json', 'w') as json_file:
@@ -68,7 +67,7 @@ def main():
 
     # change fc layer structure
     in_channel = net.fc.in_features
-    net.fc = nn.Linear(in_channel, 5)
+    net.fc = nn.Linear(in_channel, dog_list.__len__())
     net.to(device)
 
     # define loss function
